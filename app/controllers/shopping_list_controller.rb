3017bf899c50b5fa97d9db8c652767647c2e7431
Solomon <solobarine@gmail.com>
2023-06-03 13:48:32 +0100

    Update some methods

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -1,8 +1,8 @@
 class ShoppingListController < ApplicationController
-    def index
-        food_items = current_user.recipes.map(&:foods).flatten
+  skip_load_and_authorize_resource only: :index
 
-        @missing_food_items = current_user.foods - food_items
-        @total_price = @missing_food_items.sum(&:price)
-    end
+  def index
+    @shopping_list = Ingredient.joins(:recipe).where(recipes: { user_id: current_user.id }).includes(:food)
+    @shopping_list_value = @shopping_list.sum { |ingredient| ingredient.food.price * ingredient.quantity }
+  end
 end
